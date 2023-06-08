eea9ecbaa94eb97654f5c92c4508572246672a0f
muhammad arif <muhammad.arif@tbdomain.com>
2023-06-08 14:36:26 +0500

    basic calulator has been implemented using javascipt and html css

---
+++ script.js
@@ -0,0 +1,14 @@
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById("display").value = "";
+  }
+  
+  function calculate() {
+    let displayValue = document.getElementById("display").value;
+    let result = eval(displayValue);
+    document.getElementById("display").value = result;
+  }
+  
